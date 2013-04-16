a6e2aa12418c4358f410a3a49a5c687d405bbd72
Aiko Mastboom <Aiko.Mastboom@informaat.nl>
2013-04-16 21:29:49 +0200

    removed some dead code

--- preview.js
+++ preview.js
@@ -23,62 +23,14 @@ module.exports = function (config) {
 	var templateHead = Handlebars.compile(sourceHead);
 	var templateBody = Handlebars.compile(sourceBody);
 
-
-	var getCSS = function (callback) {
-		contentInstance.getAllOrderedObjects('Stylesheet', function (err, dataObjects) {
-			if (err) {
-				return callback(err);
-			}
-
-			var stylesheets = _.map(dataObjects, function(dataObject) {
-				return "\n/*	-- " + dataObject.get('title') + ' --	*/\n\n' +
-					dataObject.get('body');
-			}).join('\n');
-
-			function lessCompilationError(stylesheets, err, callback){
-				stylesheets  = "/*\n	-- The following ERROR(s) occurred during less compilation:\n\n" +
-					err.message +
-					"\n\n	-- you can add ?debug to the preview url to enable browser side less compilation." +
-					"\n\n0: this is line 0 */\n" +
-					stylesheets;
-				return callback(null, stylesheets);
-			}
-
-			try {
-				less.render(stylesheets, function (err, css) {
-					if (err) {
-						return lessCompilationError(stylesheets, err, callback);
-					}
-					return callback(err, css);
-				});
-			} catch (err){
-				return lessCompilationError(stylesheets, err, callback);
-			}
-		});
-	};
-
-	var getJS = function (callback) {
-		contentInstance.getAllOrderedObjects('Behaviour', function (err, dataObjects) {
-			if (err) {
-				return callback(err);
-			}
-
-			var scripts = _.map(dataObjects, function(dataObject) {
-				return dataObject.get('body');
-			}).join('\n');
-
-			callback(null, scripts);
-		});
-	};
-
 	var getPreviewHTML = function (options, content, callback) {
 
-		var html = replaceMarkers(content, templateHead(options), templateBody(options));
+		var html = replaceMarkers(options, content, templateHead(options), templateBody(options));
 
 		callback(null, html);
 	};
 
-	var replaceMarkers = function (html, styleMarkerReplacement, jsMarkerReplacement) {
+	var replaceMarkers = function (options, html, styleMarkerReplacement, jsMarkerReplacement) {
 
 		function replace(marker, replacement) {
 			var regExp = new RegExp('<!--\\s*@@' + marker + '\\s*-->');
@@ -89,8 +41,8 @@ module.exports = function (config) {
 			}
 		}
 
-		replace('ipe_style_marker', styleMarkerReplacement);
-		replace('ipe_js_marker', jsMarkerReplacement);
+		replace(options.collection+'_'+options.name+'_'+'_marker', styleMarkerReplacement);
+		replace(options.collection+'_'+options.name+'_'+'_marker', jsMarkerReplacement);
 
 		return html;
 	};
